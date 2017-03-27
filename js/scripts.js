6bfb69cf3a854c1746f1dad3247aa5c0c4c4a0a9
Asia Kane and Aubrey Masten <aubrey@>
2017-03-27 14:35:21 -0700

    add new method using filter to shorted inputted array

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,24 @@ $(document).ready(function(){
     event.preventDefault();
     var userInput = $("#sentence").val();
     var inputArray = userInput.split(" ");
-    var shortArray = []; //3 or more letters
+    // var shortArray = []; //3 or more letters
 
-    inputArray.forEach(function(input){
-      if(input.length >= 3){
-        shortArray.push(input);
-      }
+    // inputArray.forEach(function(input){
+    //   if(input.length >= 3){
+    //     shortArray.push(input);
+    //   }
+    // });
+
+  var shortArray = inputArray.filter(function(value){
+        return value.length >= 3;
+        //returns either true or false
+        //if true, the current element is added to shortArray
+        //if false, the current element is filtered from the array
     });
 
     var newString = shortArray.reverse().join(" ");
-    $("#output").text(newString);
-  });
+
+    $("#output1").append("This is your sentence in an array: " + inputArray);
+    $("#output2").append("This is the same array with words 3 or more letters and reversed: " + shortArray);
+    $("#output3").append("Now we've turned it back into a string: " + newString);
 });
